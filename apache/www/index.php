efd0a875cf5454bb126c57e85a73214ae9d1ca29
LoTiger <13553480577@163.com>
2019-10-02 13:17:29 +0800

    first commit

---
+++ apache/www/index.php
@@ -0,0 +1,12 @@
+<?php
+$mc = new Memcached();                 
+$mc->addServer("memcached", 11211);                                              
+$mc->add("key1", "value1");                                                        
+$mc->add("key2", "value2");                                                        
+$mc->add("key3", "value3");   
+                                                     
+echo "key1 : " . $mc->get("key1") . "\n";
+echo "key2 : " . $mc->get("key2") . "\n";
+echo "key3 : " . $mc->get("key3") . "\n";   
+phpinfo();
+
---
+++ memcached/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+memcached -d -m 128 -p 11211 -u root
